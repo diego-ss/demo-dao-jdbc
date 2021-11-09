89bf289d9b5305fa57e05f6a3f6b82087e409700
Diego <diego_sousa.s@outlook.com>
2021-11-08 21:45:27 -0300

    SellerDao FindById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,16 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.entities.Seller;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		Seller seller = sellerDao.findById(2);
+		System.out.println(seller);
 	}
 
 }
