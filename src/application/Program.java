bce420ad62648eb05ef40478fe16820cc87d06af
Diego <diego_sousa.s@outlook.com>
2021-11-09 20:09:32 -0300

    Seller update operation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,23 +11,33 @@ public class Program {
 
 	public static void main(String[] args) {
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
 		System.out.println("=== TEST 1: seller findById =====");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
+		
 		System.out.println("\n=== TEST 2: seller findByDepartment =====");
 		var sellers = sellerDao.findByDepartment(1);
 		for(Seller obj : sellers) {
 			System.out.println(obj);
 		}
+		
 		System.out.println("\n=== TEST 3: seller findAll =====");
 		sellers = sellerDao.findAll();
 		for(Seller obj : sellers) {
 			System.out.println(obj);
 		}
+		
 		System.out.println("\n=== TEST 4: seller insert =====");
 		seller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 3000.0, new Department(2, null));
 		//sellerDao.insert(seller);
 		System.out.println("Inserted! New id = " + seller.getId());
+		
+		System.out.println("\n=== TEST 5: seller update =====");
+		seller = sellerDao.findById(3);
+		seller.setName("Alex Blue");
+		//sellerDao.update(seller);
+		System.out.println("Seller updated!");
 	}
 
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -64,8 +64,35 @@ public class SellerDaoJDBC implements SellerDao{
 
 	@Override
 	public void update(Seller obj) {
-		// TODO Auto-generated method stub
+		String query = "UPDATE seller SET "
+				+ "name = ?, "
+				+ "email = ?, "
+				+ "birthdate = ?, "
+				+ "basesalary = ?, "
+				+ "departmentId = ? "
+				+ "WHERE Id = ?";
 		
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try {
+			st = conn.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+			st.setString(1, obj.getName());
+			st.setString(2, obj.getEmail());
+			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+			st.setDouble(4, obj.getBaseSalary());
+			st.setInt(5, obj.getDepartment().getId());
+			st.setInt(6, obj.getId());
+			
+			int rowsAffected = st.executeUpdate();
+
+			
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 
 	@Override
